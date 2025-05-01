f20075b8b30e7f736ceabed17a7357e6af0bd5cb
Elmo <hed-fevaleros@smu.edu.ph>
2025-05-01 17:33:56 +0800

    Add Ant Design as a dependency and clean up unused imports in App component

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import LoginButton from './components/LoginButton.jsx'
 import UserBadge from './components/UserBadge.jsx'
